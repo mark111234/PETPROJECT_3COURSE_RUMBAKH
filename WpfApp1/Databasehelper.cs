dc40b7069f8adbdda0dce70427df0dd0ec8fb7e0
Mark <147371728+mark111234@users.noreply.github.com>
2025-12-23 01:57:35 +0300

    пробуем что то новое

---
+++ WpfApp1/Databasehelper.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+namespace WpfApp1
+{
+    public class DatabaseHelper
+    {
+        private string connectionString;
+
+        public DatabaseHelper(string server, string database, string username, string password)
+        {
+            connectionString = $"Server={server};Database={database};Uid={username};Pwd={password};";
+        }
+
+        public bool TestConnection()
+        {
+            try
+            {
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,11 +1,75 @@
 ﻿using System.Windows;
 
-namespace WpfApp
+namespace WpfApp1
 {
     public partial class MainWindow : Window
     {
+        private DatabaseHelper dbHelper;
+
         public MainWindow()
         {
+            InitializeComponent();
+            dbHelper = new DatabaseHelper();
+        }
+
+        private void btnTestConnection_Click(object sender, RoutedEventArgs e)
+        {
+            if (dbHelper.TestConnection())
+            {
+                txtStatus.Text = "Подключение к MySQL серверу успешно!";
+            }
+            else
+            {
+                txtStatus.Text = "Не удалось подключиться к MySQL серверу";
+            }
+        }
+
+        private void btnCreateDB_Click(object sender, RoutedEventArgs e)
+        {
+            dbHelper.InitializeDatabase();
+            txtStatus.Text = "База данных и таблица созданы (если не существовали)";
+        }
+
+        private void btnAddNewUser_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) ||
+                string.IsNullOrWhiteSpace(txtEmail.Text) ||
+                string.IsNullOrWhiteSpace(txtAge.Text))
+            {
+                txtStatus.Text = "Заполните все поля!";
+                return;
+            }
+
+            if (!int.TryParse(txtAge.Text, out int age))
+            {
+                txtStatus.Text = "Введите корректный возраст!";
+                return;
+            }
+
+            dbHelper.AddUser(txtName.Text, txtEmail.Text, age);
+
+            // Очистка полей
+            txtName.Text = "";
+            txtEmail.Text = "";
+            txtAge.Text = "";
+        }
+
+        private void btnShowUsers_Click(object sender, RoutedEventArgs e)
+        {
+            dbHelper.GetAllUsers();
+            txtStatus.Text = "Данные пользователей выведены в консоль (View → Output)";
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtSearchEmail.Text))
+            {
+                txtStatus.Text = "Введите email для поиска!";
+                return;
+            }
+
+            dbHelper.FindUserByEmail(txtSearchEmail.Text);
+            txtSearchEmail.Text = "";
         }
     }
 }
